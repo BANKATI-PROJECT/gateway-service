40afb696c10bb7f8c2257bb47dabb51b6fa8603c
Amjad Amri <81594993+amja-do@users.noreply.github.com>
2025-01-09 00:00:22 +0100

    Update WebConfig.java

--- src/main/java/ma/ensa/gateway_service/config/WebConfig.java
+++ src/main/java/ma/ensa/gateway_service/config/WebConfig.java
@@ -2,7 +2,17 @@ package ma.ensa.gateway_service.config;
 
 
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-    
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
